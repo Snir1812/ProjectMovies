a183d32a55763afe621d06c458f1c11425bbd59e
snir agi <sniragi123@gmail.com>
2022-09-07 16:16:48 +0300

    ++

--- src/services/movies-service.js
+++ src/services/movies-service.js
@@ -5,4 +5,4 @@ export const imageUrl = (image_path) =>
   `https://image.tmdb.org/t/p/original${image_path}`;
 
 export const movieUrlCategory = (category) =>
-  `https://api.themoviedb.org/3/discover/movie?api_key=ad40cc6b3df48398b3ba8f28020bca29&language=en&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate&[19:31]%20alex%20kiochukov%20&with_genres=${category}`;
+  `https://api.themoviedb.org/3/discover/movie?api_key=ad40cc6b3df48398b3ba8f28020bca29&language=en&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate&with_genres=${category}`;
