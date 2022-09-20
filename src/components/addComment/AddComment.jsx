f0d8e8e577f3f20590d49e3892e27d23df9152fb
snir agi <sniragi123@gmail.com>
2022-09-20 15:01:02 +0300

    //

--- src/components/addComment/AddComment.jsx
+++ src/components/addComment/AddComment.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState } from "react";
+// import { useState } from "react";
 import { addComment } from "../../features/comments/comments-slice";
 import { v4 } from "uuid";
 import { useDispatch } from "react-redux";
