229fa1f2287cbe2d78585fd6c89648b7240668dc
sniragi <sniragi123@gmail.com>
2022-11-07 15:57:49 +0200

    /

--- src/components/movieItem/MovieItem.jsx
+++ src/components/movieItem/MovieItem.jsx
@@ -8,19 +8,6 @@ import AddRemoveFavoriteButton from "../AddRemoveFavoriteButton";
 
 const MovieItem = ({ movie }) => {
   const navigate = useNavigate();
-  // const style_average = {
-  //   color: "white",
-  //   fontSize: "18px",
-  //   position: "relative",
-  //   bottom: "22.4rem",
-  //   right: "5.5rem",
-  //   textShadow: "2px 2px black",
-  //   background: "rgba(0, 0, 0, 0.233)",
-  //   borderRadius: "15px",
-  //   width: "30px",
-  //   textAlign: "center",
-  // };
-  // console.log(movie.title);
 
   return (
     <div className="card-item">
