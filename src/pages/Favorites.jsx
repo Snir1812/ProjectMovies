aa600e79ad4a36416c1e31cdd945ef3b32ba5d63
sniragi <sniragi123@gmail.com>
2022-11-15 19:35:09 +0200

    css upgrades

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -3,18 +3,32 @@
 import { useSelector } from "react-redux";
 import MovieItem from "../components/movieItem/MovieItem";
 import "../components/Movies.css";
+import "../components/GeneralPageCss.css";
+import { useNavigate } from "react-router-dom";
 
 const Favorites = () => {
   const movies = useSelector((s) => s.movie.search).filter((m) => m.isFavorite);
-  /**const [movies, setMovies] = useState([]);
-  useEffect(() => {
-    const saved = localStorage.getItem("movies");
-    if (saved) {
-      setMovies(JSON.parse(saved));
-    }
-  }, []);
-  **/
-  return (
+  const darkToggle = useSelector((state) => state.theme);
+  const navigate = useNavigate();
+
+  return movies < 1 ? (
+    <div
+      className={darkToggle.darkTheme ? "general-page-dark" : "general-page"}
+    >
+      <p>The favorites page is empty</p>
+      <p>
+        To add movies to your favorites , click on the button and select Movies
+      </p>
+      <button
+        className="btn btn-back"
+        onClick={() => {
+          navigate("/movies");
+        }}
+      >
+        Click Me
+      </button>
+    </div>
+  ) : (
     <div className="container">
       <div className="card-list">
         {movies.map((m) => (
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,21 @@
 import React from "react";
 import "../components/GeneralPageCss.css";
+import { useSelector } from "react-redux";
 
 const Home = () => {
-  return <div className="container general-page">Home</div>;
+  const darkToggle = useSelector((state) => state.theme);
+
+  return (
+    <div
+      className={
+        darkToggle.darkTheme
+          ? "container general-page-dark"
+          : "container general-page"
+      }
+    >
+      Home
+    </div>
+  );
 };
 
 export default Home;
